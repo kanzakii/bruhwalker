afd1d6683e17d136a9ab12acd39f1befd2675677
kazumi <38107033+xkazumi@users.noreply.github.com>
2021-03-03 17:01:45 +0800

    Update test.lua

--- test auto update/test.lua
+++ test auto update/test.lua
@@ -1,5 +1,5 @@
-web_version = http:get("https://raw.githubusercontent.com/xkazumi/bruhwalker-dragon-tracker/main/version.txt")
+web_version = http:get("https://raw.githubusercontent.com/xkazumi/bruhwalker/main/test%20auto%20update/test.version")
 if web_version ~= "v2" then
     console:log("updating")
-    http:download_file("https://raw.githubusercontent.com/xkazumi/bruhwalker-dragon-tracker/main/version.txt", "test.lua")
+    http:download_file("https://raw.githubusercontent.com/xkazumi/bruhwalker/main/test%20auto%20update/test.lua", "test.lua")
 end
