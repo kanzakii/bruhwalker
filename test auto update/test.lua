93cf9b00889891feda365b16fb96a5fa39d94860
kazumi <38107033+xkazumi@users.noreply.github.com>
2021-03-03 17:04:42 +0800

    Update test.lua

--- test auto update/test.lua
+++ test auto update/test.lua
@@ -1,7 +1,7 @@
 web_version = http:get("https://raw.githubusercontent.com/xkazumi/bruhwalker/main/test%20auto%20update/test.version")
 if web_version ~= "v2" then
-    console:log("updating")
     http:download_file("https://raw.githubusercontent.com/xkazumi/bruhwalker/main/test%20auto%20update/test.lua", "test.lua")
+    console:log("updated test.lua")
 end
 
 console:log("test v2")
